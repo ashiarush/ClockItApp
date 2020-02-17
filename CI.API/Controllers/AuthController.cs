c2cbd4eaf6be3291204a13f4dc1c947281e7d39a
Alok Gupta <Alok.Gupta@excelian.com>
2020-02-17 22:12:07 +0530

    Commit before changing key in appsettings.json

---
+++ CI.API/Controllers/AuthController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using CI.API.ViewModels;
+using CI.DAL;
+using CI.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IConfiguration _config;
+
+        public AuthController(UserManager<User> userManager, SignInManager<User> signInManager, IConfiguration config)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _config = config;
+        }
+        
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+
+            if(user==null)
+            {
+                return BadRequest();
+            }
+
+            var result=await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+
+            if(!result.Succeeded)
+            {
+                return BadRequest(result);
+            }
+            return Ok(new { 
+                result = result,
+                token = JwtTokenGeneratorMachine(user)
+            });
+        }
+
+        private async Task<string> JwtTokenGeneratorMachine(User userInfo)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, userInfo.Id),
+                new Claim(ClaimTypes.Name, userInfo.UserName)
+            };
+
+            var roles = await _userManager.GetRolesAsync(userInfo);
+
+            foreach(var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _config.GetSection("AppSettings:Key").Value));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ CI.API/Controllers/EmployersController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CI.API.ViewModels;
+using CI.DAL;
+using CI.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployersController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public EmployersController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        
+        [HttpPost("create")]
+        public async Task<ActionResult> Create(CreateEmployerViewModel model)
+        {
+            if(!(await _roleManager.RoleExistsAsync("Employer")))
+            {
+                await _roleManager.CreateAsync(new IdentityRole("Employer"));
+            }
+
+            var employer=new User{ 
+                UserName=model.Username,
+                Email=model.Email
+            };
+
+            var result=await _userManager.CreateAsync(employer, model.Password);
+
+            if(!result.Succeeded)
+            {
+                return BadRequest(result);
+            }
+
+            var userFromDb = await _userManager.FindByNameAsync(employer.UserName);
+            await _userManager.AddToRoleAsync(userFromDb, "Employer");  
+
+            return Ok(result);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
