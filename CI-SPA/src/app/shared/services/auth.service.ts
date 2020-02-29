0bf317b8a9b13793a60fa30ff3bfb3c4df23cb89
Alok Gupta <Alok.Gupta@excelian.com>
2020-02-29 11:00:17 +0530

    Till Bootwatch.

--- CI-SPA/src/app/shared/services/auth.service.ts
+++ CI-SPA/src/app/shared/services/auth.service.ts
@@ -1,38 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from "@auth0/angular-jwt";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  authUrl = 'http://localhost:5000/api/auth/';
-  employerUrl = 'http://localhost:5000/api/employers/';
-  confirmEmailUrl = 'http://localhost:4200/confirm-email/';
-  changePasswordUrl = 'http://localhost:4200/change-password/';
+  authUrl = "http://localhost:5000/api/auth/";
+  employersUrl = "http://localhost:5000/api/employers/";
+  confirmEmailUrl = "http://localhost:4200/confirm-email/";
+  changePasswordUrl = "http://localhost:4200/change-password/";
+  helper = new JwtHelperService();
+  decodedToken: any;
 
   constructor(private http: HttpClient) { }
 
-  login(model: any){
+  login(model: any) {
     return this.http.post(this.authUrl + 'login', model).pipe(
       map((response: any) => {
         const user = response;
         if (user.result.succeeded) {
           localStorage.setItem('token', user.token);
+          this.decodedToken = this.helper.decodeToken(user.token);
         }
       })
-    );
+    )
   }
 
-  register(model: any){
+  register(model: any) {
     let headers = new HttpHeaders({
       'confirmEmailUrl': this.confirmEmailUrl
     });
-
-    let options = {headers};
-
-    return this.http.post(this.employerUrl + 'create', model, options);
+    let options = { headers: headers };
+    return this.http.post(this.employersUrl + 'create', model, options);
   }
 
   resetPassword(model: any) {
@@ -46,8 +49,15 @@ export class AuthService {
   confirmEmail(model: any) {
     return this.http.post(this.authUrl + 'confirmemail', model);
   }
-  
+
   changePassword(model: any) {
     return this.http.post(this.authUrl + 'changepassword', model);
   }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.helper.isTokenExpired(token);
+  }
+
+
 }
